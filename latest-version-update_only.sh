4be0956cebc9af8d606f257e72d5530bb7f50a66
CHANDRASHEKAR <devops16008121@gmail.com>
2025-10-07 20:45:01 +0530

    Update latest-version-update_only.sh

--- latest-version-update_only.sh
+++ latest-version-update_only.sh
@@ -3,4 +3,3 @@ echo "🚀 FULL PROMOTION TO v1.1"
 echo "100% traffic → v1.1 (Latest - Green)"
 kubectl apply -f istio-config/latest-verion-update_only.yaml
 echo "✅ Full promotion complete! All traffic now goes to latest v1.1"
-./test-traffic-split.sh
